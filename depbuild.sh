9813269811f6f971b012c543319dc946aa6fda29
James Sutherland <james@deadnode.org>
2011-05-19 16:30:53 +0100

    Get shell script building dependencies into deps instead of systemwide

--- depbuild.sh
+++ depbuild.sh
@@ -2,27 +2,20 @@
 
 cd deps &&
 
-tar xzvf ldns-1.6.5.tar.gz &&
 cd ldns-1.6.5 &&
-./configure &&
+./configure --prefix=`pwd`/.. --enable-static --disable-shared &&
 make &&
 make install &&
-cd .. &&
-tar xzvf libevent-2.0.1-alpha.tar.gz &&
-cd libevent-2.0.1-alpha &&
-./configure &&
+cd ../libevent-2.0.1-alpha &&
+./configure --prefix=`pwd`/.. --enable-static --disable-shared &&
 make &&
 make install &&
-cd .. &&
-tar xzvf libghthash-0.6.2.tar.gz &&
-cd libghthash-0.6.2 &&
-./configure &&
+cd ../libghthash-0.6.2 &&
+./configure --prefix=`pwd`/.. --enable-static --disable-shared &&
 make &&
 make install &&
-cd .. &&
-tar xzvf unbound-1.4.3_lite.tar.gz &&
-cd unbound-1.4.3 &&
-./configure &&
+cd ../unbound-1.4.3 &&
+./configure --prefix=`pwd`/.. --enable-static --disable-shared &&
 make &&
 make install &&
 cd .. &&
